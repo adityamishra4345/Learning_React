6fd82389e5da53b3aec4666b21a7fdf813769fe3
adityamishra4345 <adityamishra4345@gmail.com>
2026-06-30 23:12:49 +0530

    counter updated

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -9,11 +9,15 @@ import './App.css'
   const [count, setCount] = useState(15)
     const increase=function inccounter(){
       if(count<25){
+        //if i write 10 times this then also only 1 added
+        //to do setCounter(Counter => Counter + 1)
+         
         setCount(count+1);}
 
   }
    const decrese=function deccounter(){
     if(count>0){
+        
         setCount(count-1);}
 
   }
@@ -21,9 +25,9 @@ import './App.css'
   return (
     <>
       <h1 >count is {count}</h1>
-      <button id="but" onClick={increase} > Increse </button>
+      <button class="but" onClick={increase} > Increse </button>
       <br />
-      <button id="but" onClick={decrese}>Decrese</button>
+      <button class="but" onClick={decrese}>Decrese</button>
     </>
   )
 }
